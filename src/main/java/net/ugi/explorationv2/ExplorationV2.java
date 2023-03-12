5fced0ccd1f3b8703b9ba28e9d34039e4430dcf6
Matteo <matteo.feyaerts@gmail.com>
2023-03-12 16:12:41 +0100

    added citrine as example

--- src/main/java/net/ugi/explorationv2/ExplorationV2.java
+++ src/main/java/net/ugi/explorationv2/ExplorationV2.java
@@ -1,6 +1,8 @@
 package net.ugi.explorationv2;
 
 import net.fabricmc.api.ModInitializer;
+import net.ugi.explorationv2.item.ModItemGroup;
+import net.ugi.explorationv2.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,6 +12,8 @@ public class ExplorationV2 implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItemGroup.registerItemgroups();
+		ModItems.registerModItems();
 		LOGGER.info("explorationv2 has loaded");
 	}
 }
