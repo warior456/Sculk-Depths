5f620bd4d5dc13983719f4de9fd29b204daf5759
gemsb <qvdschrick@gmail.com>
2023-03-18 09:36:21 +0100

    sculk fluid

---
+++ src/main/java/net/ugi/sculk_depths/fluid/ModFluids.java
@@ -0,0 +1,39 @@
+package net.ugi.sculk_depths.fluid;
+
+import com.google.common.collect.UnmodifiableIterator;
+import net.minecraft.fluid.FlowableFluid;
+import net.minecraft.fluid.Fluid;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.ugi.sculk_depths.SculkDepths;
+
+import java.util.Iterator;
+
+public class ModFluids {
+    public static final FlowableFluid FLOWING_SCULK_FLUID = register("flowing_sculk_fluid", new SculkFluidFluid.Flowing());
+    public static final FlowableFluid SCULK_FLUID = register("sculk_fluid", new SculkFluidFluid.Still());
+
+
+    private static <T extends Fluid> T register(String id, T value) {
+        return Registry.register(Registries.FLUID, id, value);
+    }
+
+    static {
+        Iterator var0 = Registries.FLUID.iterator();
+
+        while(var0.hasNext()) {
+            Fluid fluid = (Fluid)var0.next();
+            UnmodifiableIterator var2 = fluid.getStateManager().getStates().iterator();
+
+            while(var2.hasNext()) {
+                FluidState fluidState = (FluidState)var2.next();
+                Fluid.STATE_IDS.add(fluidState);
+            }
+        }
+
+    }
+    public static void registerModFluids() {
+        SculkDepths.LOGGER.info("Registering ModFluids for " + SculkDepths.MOD_ID);
+    }
+}
---
+++ src/main/java/net/ugi/sculk_depths/fluid/SculkFluidFluid.java
@@ -0,0 +1,135 @@
+package net.ugi.sculk_depths.fluid;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.FluidBlock;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.fluid.FlowableFluid;
+import net.minecraft.fluid.Fluid;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import net.minecraft.particle.ParticleEffect;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.registry.tag.FluidTags;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.Property;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.world.*;
+import net.ugi.sculk_depths.block.ModBlocks;
+import net.ugi.sculk_depths.item.ModItems;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Optional;
+
+public abstract class SculkFluidFluid extends FlowableFluid {
+    public SculkFluidFluid() {
+    }
+
+    public Fluid getFlowing() {
+        return ModFluids.FLOWING_SCULK_FLUID;
+    }
+
+    public Fluid getStill() {
+        return ModFluids.SCULK_FLUID;
+    }
+
+    public Item getBucketItem() {
+        return ModItems.SCULK_FLUID_BUCKET;
+    }
+
+    public void randomDisplayTick(World world, BlockPos pos, FluidState state, Random random) {
+        if (!state.isStill() && !(Boolean)state.get(FALLING)) {
+            if (random.nextInt(64) == 0) {
+                world.playSound((double)pos.getX() + 0.5, (double)pos.getY() + 0.5, (double)pos.getZ() + 0.5, SoundEvents.BLOCK_WATER_AMBIENT, SoundCategory.BLOCKS, random.nextFloat() * 0.25F + 0.75F, random.nextFloat() + 0.5F, false);
+            }
+        } else if (random.nextInt(10) == 0) {
+            world.addParticle(ParticleTypes.UNDERWATER, (double)pos.getX() + random.nextDouble(), (double)pos.getY() + random.nextDouble(), (double)pos.getZ() + random.nextDouble(), 0.0, 0.0, 0.0);
+        }
+
+    }
+
+    @Nullable
+    public ParticleEffect getParticle() {
+        return ParticleTypes.DRIPPING_WATER;
+    }
+
+    protected boolean isInfinite(World world) {
+        return world.getGameRules().getBoolean(GameRules.WATER_SOURCE_CONVERSION);
+    }
+
+    protected void beforeBreakingBlock(WorldAccess world, BlockPos pos, BlockState state) {
+        BlockEntity blockEntity = state.hasBlockEntity() ? world.getBlockEntity(pos) : null;
+        Block.dropStacks(state, world, pos, blockEntity);
+    }
+
+    public int getFlowSpeed(WorldView world) {
+        return 4;
+    }
+
+    public BlockState toBlockState(FluidState state) {
+        return ModBlocks.SCULK_FLUID.getDefaultState().with(FluidBlock.LEVEL, getBlockStateLevel(state));
+    }
+
+    public boolean matchesType(Fluid fluid) {
+        return fluid == ModFluids.SCULK_FLUID || fluid == ModFluids.FLOWING_SCULK_FLUID;
+    }
+
+    public int getLevelDecreasePerBlock(WorldView world) {
+        return 1;
+    }
+
+    public int getTickRate(WorldView world) {
+        return 5;
+    }
+
+    public boolean canBeReplacedWith(FluidState state, BlockView world, BlockPos pos, Fluid fluid, Direction direction) {
+        return direction == Direction.DOWN;
+    }
+
+    protected float getBlastResistance() {
+        return 100.0F;
+    }
+
+    public Optional<SoundEvent> getBucketFillSound() {
+        return Optional.of(SoundEvents.ITEM_BUCKET_FILL);
+    }
+
+    public static class Flowing extends net.ugi.sculk_depths.fluid.SculkFluidFluid {
+        public Flowing() {
+        }
+
+        protected void appendProperties(StateManager.Builder<Fluid, FluidState> builder) {
+            super.appendProperties(builder);
+            builder.add(new Property[]{LEVEL});
+        }
+
+        public int getLevel(FluidState state) {
+            return state.get(LEVEL);
+        }
+
+        public boolean isStill(FluidState state) {
+            return false;
+        }
+    }
+
+    public static class Still extends net.ugi.sculk_depths.fluid.SculkFluidFluid {
+        public Still() {
+        }
+
+        public int getLevel(FluidState state) {
+            return 8;
+        }
+
+        public boolean isStill(FluidState state) {
+            return true;
+        }
+    }
+}
