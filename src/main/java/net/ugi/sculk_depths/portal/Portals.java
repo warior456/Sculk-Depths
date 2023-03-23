1a24f8671ac8df82e19f75830f32daadaf95ce36
Peter Zmatek <xzmatek@musa25.fi.muni.cz>
2023-03-23 18:31:31 +0100

    Updated CustomPortalAPI

--- src/main/java/net/ugi/sculk_depths/portal/Portals.java
+++ src/main/java/net/ugi/sculk_depths/portal/Portals.java
@@ -15,7 +15,7 @@ public class Portals {
                 .destDimID(new Identifier("sculk_depths:sculk_depthsdim"))
                 .tintColor(0, 115, 150)
                 .lightWithItem(ModItems.SOUL_HEART)
-                // .setPortalSearchYRange(0,120)
+                .setPortalSearchYRange(0,120)
                 .registerIgniteEvent((player, world, portalPos, framePos, portalIgnitionSource) -> {
                     if (portalIgnitionSource.sourceType == PortalIgnitionSource.SourceType.USEITEM && player != null) {
                         if (player.isCreative())
