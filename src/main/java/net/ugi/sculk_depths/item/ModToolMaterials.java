2938b05faa91eb23e17b479bc423177ef32e44ac
Matteo <matteo.feyaerts@gmail.com>
2023-03-27 21:00:27 +0200

    quazarith pickaxe

--- src/main/java/net/ugi/sculk_depths/item/ModToolMaterials.java
+++ src/main/java/net/ugi/sculk_depths/item/ModToolMaterials.java
@@ -12,14 +12,14 @@ import java.util.function.Supplier;
 public enum ModToolMaterials implements ToolMaterial
 {
     //GOLD(MiningLevels.WOOD, 32, 12.0f, 0.0f, 22, () -> Ingredient.ofItems(Items.GOLD_INGOT)),
-    QUAZARITH(6, 3000, 12.0f, 4.0f, 30, () -> Ingredient.ofItems(ModItems.QUAZARITH)); //change to quazarith ingot
-
+    QUAZARITH(6, 3000, 38.29f, 4.0f, 30, () -> Ingredient.ofItems(ModItems.QUAZARITH)); //change to quazarith ingot
+    //deepslate instamine miningspeed: 38.29
     private final int miningLevel;
     private final int itemDurability;
     private final float miningSpeed;
     private final float attackDamage;
     private final int enchantability;
-    private final Lazy<Ingredient> repairIngredient;
+    private final Supplier<Ingredient> repairIngredient;
 
     private ModToolMaterials(int miningLevel, int itemDurability, float miningSpeed, float attackDamage, int enchantability, Supplier<Ingredient> repairIngredient) {
         this.miningLevel = miningLevel;
@@ -27,7 +27,7 @@ public enum ModToolMaterials implements ToolMaterial
         this.miningSpeed = miningSpeed;
         this.attackDamage = attackDamage;
         this.enchantability = enchantability;
-        this.repairIngredient = new Lazy<Ingredient>(repairIngredient);
+        this.repairIngredient = repairIngredient;
     }
 
     @Override
