802dfe4250d03501a87c9db86df1e5424f8b3767
Matteo <matteo.feyaerts@gmail.com>
2023-04-02 19:40:27 +0200

    fix music

--- src/main/java/net/ugi/sculk_depths/SculkDepths.java
+++ src/main/java/net/ugi/sculk_depths/SculkDepths.java
@@ -1,10 +1,15 @@
 package net.ugi.sculk_depths;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.util.Identifier;
 import net.ugi.sculk_depths.block.ModBlocks;
 import net.ugi.sculk_depths.item.ModItemGroup;
 import net.ugi.sculk_depths.item.ModItems;
 import net.ugi.sculk_depths.portal.Portals;
+import net.ugi.sculk_depths.sound.ModSounds;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,12 +17,19 @@ public class SculkDepths implements ModInitializer {
     public static final String MOD_ID = "sculk_depths";
     public static final Logger LOGGER = LoggerFactory.getLogger("sculk_depths");
 
+
+
+
+
     @Override
     public void onInitialize() {
         ModItemGroup.registerItemgroups();
         ModItems.registerModItems();
         ModBlocks.registerModBlocks();
         Portals.registerModPortals();
+        ModSounds.registerModSounds();
+
+
         LOGGER.info("sculk_depths has loaded");
     }
 }
