3187d0851de4788006a2d20edf660070ecad42ba
Matteo <matteo.feyaerts@gmail.com>
2023-04-26 14:57:03 +0200

    fix sapling transparency

--- src/main/java/net/ugi/sculk_depths/SculkDepthsClient.java
+++ src/main/java/net/ugi/sculk_depths/SculkDepthsClient.java
@@ -1,10 +1,13 @@
 package net.ugi.sculk_depths;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.render.fluid.v1.FluidRenderHandlerRegistry;
 import net.fabricmc.fabric.api.client.render.fluid.v1.SimpleFluidRenderHandler;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+import net.minecraft.client.render.RenderLayer;
 import net.minecraft.util.Identifier;
+import net.ugi.sculk_depths.block.ModBlocks;
 import net.ugi.sculk_depths.entity.ModEntities;
 import net.ugi.sculk_depths.entity.client.GlomperRenderer;
 import net.ugi.sculk_depths.fluid.ModFluids;
@@ -22,6 +25,8 @@ public class SculkDepthsClient implements ClientModInitializer {
                         new Identifier("sculk_depths:block/kryslum_flow")
                 )
         );
+
         EntityRendererRegistry.register(ModEntities.GLOMPER, GlomperRenderer::new);
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.VALTROX_SAPLING, RenderLayer.getCutout());
     }
 }
