2a7032b76def3a618f25bd180cac084f1ced1e0c
Matteo <matteo.feyaerts@gmail.com>
2023-06-02 22:12:21 +0200

    generators removed and disabled because they do not work

--- src/main/java/net/ugi/sculk_depths/entity/custom/GlomperEntity.java
+++ src/main/java/net/ugi/sculk_depths/entity/custom/GlomperEntity.java
@@ -1,7 +1,6 @@
 package net.ugi.sculk_depths.entity.custom;
 
 
-import net.minecraft.entity.EntityGroup;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.control.FlightMoveControl;
@@ -15,11 +14,8 @@ import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.mob.PathAwareEntity;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.predicate.entity.EntityPredicates;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import net.minecraft.world.WorldView;
 import net.ugi.sculk_depths.entity.ModEntities;
 import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib.animatable.GeoEntity;
