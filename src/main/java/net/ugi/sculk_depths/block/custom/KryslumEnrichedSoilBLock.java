440fea9f6e2ed19a6f53ade80bde60745585cac6
Matteo <matteo.feyaerts@gmail.com>
2023-06-17 23:24:57 +0200

    main to 1.20

--- src/main/java/net/ugi/sculk_depths/block/custom/KryslumEnrichedSoilBLock.java
+++ src/main/java/net/ugi/sculk_depths/block/custom/KryslumEnrichedSoilBLock.java
@@ -1,13 +1,8 @@
 package net.ugi.sculk_depths.block.custom;
 
-import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.*;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.ai.pathing.NavigationType;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemPlacementContext;
-import net.minecraft.registry.tag.FluidTags;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.IntProperty;
@@ -17,9 +12,6 @@ import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.*;
-import net.minecraft.world.event.GameEvent;
-import net.ugi.sculk_depths.block.ModBlocks;
-import org.jetbrains.annotations.Nullable;
 
 public class KryslumEnrichedSoilBLock extends FarmlandBlock {
 
@@ -27,9 +19,9 @@ public class KryslumEnrichedSoilBLock extends FarmlandBlock {
     protected static final VoxelShape SHAPE = Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 15.0, 16.0);
     public static final int MAX_MOISTURE = 7;
 
-    public KryslumEnrichedSoilBLock(FabricBlockSettings settings) {
+    public KryslumEnrichedSoilBLock(AbstractBlock.Settings settings) {
         super(settings);
-        this.setDefaultState((BlockState)((BlockState)this.stateManager.getDefaultState()).with(MOISTURE, 7));
+        this.setDefaultState((BlockState)((BlockState)this.stateManager.getDefaultState()).with(MOISTURE, MAX_MOISTURE));
     }
 
     @Override
@@ -40,11 +32,6 @@ public class KryslumEnrichedSoilBLock extends FarmlandBlock {
         return super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos);
     }
 
-    @Override
-    public boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
-        BlockState blockState = world.getBlockState(pos.up());
-        return !blockState.getMaterial().isSolid() || blockState.getBlock() instanceof FenceGateBlock || blockState.getBlock() instanceof PistonExtensionBlock;
-    }
 
     @Override
     public BlockState getPlacementState(ItemPlacementContext ctx) {
@@ -54,10 +41,6 @@ public class KryslumEnrichedSoilBLock extends FarmlandBlock {
         return super.getPlacementState(ctx);
     }
 
-    @Override
-    public boolean hasSidedTransparency(BlockState state) {
-        return true;
-    }
 
     @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
@@ -66,9 +49,6 @@ public class KryslumEnrichedSoilBLock extends FarmlandBlock {
 
     @Override
     public void scheduledTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
-        if (!state.canPlaceAt(world, pos)) {
-            FarmlandBlock.setToDirt(null, state, world, pos);
-        }
     }
 
     @Override
@@ -77,9 +57,7 @@ public class KryslumEnrichedSoilBLock extends FarmlandBlock {
 
     @Override
     public void onLandedUpon(World world, BlockState state, BlockPos pos, Entity entity, float fallDistance) {
-
     }
-
 
 
 
@@ -88,8 +66,4 @@ public class KryslumEnrichedSoilBLock extends FarmlandBlock {
         builder.add(MOISTURE);
     }
 
-    @Override
-    public boolean canPathfindThrough(BlockState state, BlockView world, BlockPos pos, NavigationType type) {
-        return false;
-    }
 }
