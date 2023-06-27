697fbe5601463fbfacdace89b1b03af172b8c167
Matteo <matteo.feyaerts@gmail.com>
2023-06-27 17:01:48 +0200

    add energy essence drop for entities with drops_energy_essence

---
+++ src/main/java/net/ugi/sculk_depths/tags/ModEntityTags.java
@@ -0,0 +1,11 @@
+package net.ugi.sculk_depths.tags;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.Identifier;
+import net.ugi.sculk_depths.SculkDepths;
+
+public class ModEntityTags {
+        public static final TagKey<EntityType<?>> DROPS_ENERGY_ESSENCE = TagKey.of(RegistryKeys.ENTITY_TYPE, new Identifier(SculkDepths.MOD_ID, "drops_energy_essence"));
+}
