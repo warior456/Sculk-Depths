37ef36e64850cf01fa3e22177c0062e4cbf6a94a
Matteo <matteo.feyaerts@gmail.com>
2023-06-28 15:38:15 +0200

    fix lava no longer spawning at the bottom of the overworld

--- src/main/java/net/ugi/sculk_depths/mixin/NoiseChunkGeneratorMixin.java
+++ src/main/java/net/ugi/sculk_depths/mixin/NoiseChunkGeneratorMixin.java
@@ -1,20 +1,37 @@
 package net.ugi.sculk_depths.mixin;
 
+import net.minecraft.block.Blocks;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldProperties;
+import net.minecraft.world.dimension.DimensionType;
 import net.minecraft.world.gen.chunk.AquiferSampler;
 import net.minecraft.world.gen.chunk.ChunkGeneratorSettings;
 import net.minecraft.world.gen.chunk.NoiseChunkGenerator;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
 
 
 @Mixin({NoiseChunkGenerator.class})
-public class NoiseChunkGeneratorMixin {
+public abstract class NoiseChunkGeneratorMixin {
+    @Shadow public abstract RegistryEntry<ChunkGeneratorSettings> getSettings();
+
     /**
-     * @author Andrew6rant (Andrew Grant)
+     * @author Matteo_fey (@warior456)
      * @reason Remove the hardcoded -54 lava sea level
      */
     @Overwrite
     private static AquiferSampler.FluidLevelSampler createFluidLevelSampler(ChunkGeneratorSettings settings) {
-        return (x, y, z) -> new AquiferSampler.FluidLevel(settings.seaLevel(), settings.defaultFluid());
+        AquiferSampler.FluidLevel fluidLevel = new AquiferSampler.FluidLevel(settings.seaLevel() - 117, Blocks.LAVA.getDefaultState());
+        int i = settings.seaLevel();
+        AquiferSampler.FluidLevel fluidLevel2 = new AquiferSampler.FluidLevel(i, settings.defaultFluid());
+        AquiferSampler.FluidLevel fluidLevel3 = new AquiferSampler.FluidLevel(DimensionType.MIN_HEIGHT * 2, Blocks.AIR.getDefaultState());
+        return (x, y, z) -> {
+            if (y < Math.min(settings.seaLevel() -117 , i)) { //todo configurable
+                return fluidLevel;
+            }
+            return fluidLevel2;
+        };
     }
 }
