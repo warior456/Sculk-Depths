99ad2c5e2219a7ec5c2db74c28455115b6046a75
Matteo <matteo.feyaerts@gmail.com>
2023-08-16 22:13:14 +0200

    added some stuff to datagen

--- src/main/java/net/ugi/sculk_depths/datagen/ModModelProvider.java
+++ src/main/java/net/ugi/sculk_depths/datagen/ModModelProvider.java
@@ -5,6 +5,7 @@ import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
 import net.minecraft.data.client.BlockStateModelGenerator;
 import net.minecraft.data.client.ItemModelGenerator;
 import net.minecraft.data.client.Models;
+import net.ugi.sculk_depths.block.ModBlocks;
 
 public class ModModelProvider extends FabricModelProvider {
     public ModModelProvider(FabricDataOutput output) {
@@ -16,6 +17,12 @@ public class ModModelProvider extends FabricModelProvider {
         /*
         blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.RUBY_BLOCK);
          */
+        BlockStateModelGenerator.BlockTexturePool valtroxPool = blockStateModelGenerator.registerCubeAllModelTexturePool(ModBlocks.VALTROX_PLANKS);
+        BlockStateModelGenerator.BlockTexturePool driedValtroxPool = blockStateModelGenerator.registerCubeAllModelTexturePool(ModBlocks.DRIED_VALTROX_PLANKS);
+
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.PENEBRIUM_SHINE_SHROOM_BLOCK);
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.PENEBRIUM_SHINE_SHROOM_SPORE_BLOCK);
+
 
     }
 
