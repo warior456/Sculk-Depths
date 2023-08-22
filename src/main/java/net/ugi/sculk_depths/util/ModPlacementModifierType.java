7d3be685498a77fbd0ff10bca2378ac62782af84
Matteo <matteo.feyaerts@gmail.com>
2023-08-22 22:13:57 +0200

    ModPlacementModifierType registry is now under the correct namespace

--- src/main/java/net/ugi/sculk_depths/util/ModPlacementModifierType.java
+++ src/main/java/net/ugi/sculk_depths/util/ModPlacementModifierType.java
@@ -4,17 +4,18 @@ package net.ugi.sculk_depths.util;
 import com.mojang.serialization.Codec;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import net.minecraft.world.gen.placementmodifier.PlacementModifier;
 import net.minecraft.world.gen.placementmodifier.PlacementModifierType;
 import net.ugi.sculk_depths.SculkDepths;
 
   public interface ModPlacementModifierType<P extends PlacementModifier> {
 
-      PlacementModifierType<CountOnEveryLayerConstant> COUNT_ON_EVERY_LAYER_CONSTANT = register( "sculk_depths__count_on_every_layer_constant", CountOnEveryLayerConstant.MODIFIER_CODEC);
+      PlacementModifierType<CountOnEveryLayerConstant> COUNT_ON_EVERY_LAYER_CONSTANT = register( "count_on_every_layer_constant", CountOnEveryLayerConstant.MODIFIER_CODEC);
 
 
       private static <P extends PlacementModifier> PlacementModifierType<P> register (String id, Codec<P> codec) {
-          return Registry.register(Registries.PLACEMENT_MODIFIER_TYPE, id, () -> codec);
+          return Registry.register(Registries.PLACEMENT_MODIFIER_TYPE, new Identifier(SculkDepths.MOD_ID, id), () -> codec);
       }
 
       public static void init () {
