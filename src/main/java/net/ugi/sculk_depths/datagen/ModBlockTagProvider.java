120ba56b00a86abce340c28e3c5f8eab8dc6bd5a
warior456 <matteo.feyaerts@gmail.com>
2023-09-18 00:06:53 +0200

    sign tags + datagen

--- src/main/java/net/ugi/sculk_depths/datagen/ModBlockTagProvider.java
+++ src/main/java/net/ugi/sculk_depths/datagen/ModBlockTagProvider.java
@@ -4,9 +4,11 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.registry.tag.TagKey;
 import net.minecraft.util.Identifier;
 import net.ugi.sculk_depths.block.ModBlocks;
+import software.bernie.shadowed.eliotlash.mclib.math.functions.classic.Mod;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -48,6 +50,71 @@ public class ModBlockTagProvider extends FabricTagProvider.BlockTagProvider {
                 .add(ModBlocks.QUAZARITH_ORE);
 
 
+        getOrCreateTagBuilder(BlockTags.ALL_SIGNS)
+                .add(ModBlocks.VALTROX_SIGN)
+                .add(ModBlocks.VALTROX_WALL_SIGN)
+                .add(ModBlocks.VALTROX_HANGING_SIGN)
+                .add(ModBlocks.VALTROX_WALL_HANGING_SIGN)
 
+                .add(ModBlocks.DRIED_VALTROX_SIGN)
+                .add(ModBlocks.DRIED_VALTROX_WALL_SIGN)
+                .add(ModBlocks.DRIED_VALTROX_HANGING_SIGN)
+                .add(ModBlocks.DRIED_VALTROX_WALL_HANGING_SIGN)
+
+                .add(ModBlocks.PETRIFIED_VALTROX_SIGN)
+                .add(ModBlocks.PETRIFIED_VALTROX_WALL_SIGN)
+                .add(ModBlocks.PETRIFIED_VALTROX_HANGING_SIGN)
+                .add(ModBlocks.PETRIFIED_VALTROX_WALL_HANGING_SIGN);
+
+
+        getOrCreateTagBuilder(BlockTags.SIGNS)
+                .add(ModBlocks.VALTROX_SIGN)
+                .add(ModBlocks.VALTROX_WALL_SIGN)
+
+                .add(ModBlocks.DRIED_VALTROX_SIGN)
+                .add(ModBlocks.DRIED_VALTROX_WALL_SIGN)
+
+                .add(ModBlocks.PETRIFIED_VALTROX_SIGN)
+                .add(ModBlocks.PETRIFIED_VALTROX_WALL_SIGN);
+
+
+        getOrCreateTagBuilder(BlockTags.STANDING_SIGNS)
+                .add(ModBlocks.VALTROX_SIGN)
+
+                .add(ModBlocks.DRIED_VALTROX_SIGN)
+
+                .add(ModBlocks.PETRIFIED_VALTROX_SIGN);
+
+        getOrCreateTagBuilder(BlockTags.WALL_SIGNS)
+                .add(ModBlocks.VALTROX_WALL_SIGN)
+
+                .add(ModBlocks.DRIED_VALTROX_WALL_SIGN)
+
+                .add(ModBlocks.PETRIFIED_VALTROX_WALL_SIGN);
+
+        getOrCreateTagBuilder(BlockTags.ALL_HANGING_SIGNS)
+                .add(ModBlocks.VALTROX_HANGING_SIGN)
+                .add(ModBlocks.VALTROX_WALL_HANGING_SIGN)
+
+                .add(ModBlocks.DRIED_VALTROX_HANGING_SIGN)
+                .add(ModBlocks.DRIED_VALTROX_WALL_HANGING_SIGN)
+
+                .add(ModBlocks.PETRIFIED_VALTROX_HANGING_SIGN)
+                .add(ModBlocks.PETRIFIED_VALTROX_WALL_HANGING_SIGN);
+
+        getOrCreateTagBuilder(BlockTags.CEILING_HANGING_SIGNS)
+                .add(ModBlocks.VALTROX_HANGING_SIGN)
+
+                .add(ModBlocks.DRIED_VALTROX_HANGING_SIGN)
+
+                .add(ModBlocks.PETRIFIED_VALTROX_HANGING_SIGN);
+
+
+        getOrCreateTagBuilder(BlockTags.WALL_HANGING_SIGNS)
+                .add(ModBlocks.VALTROX_WALL_HANGING_SIGN)
+
+                .add(ModBlocks.DRIED_VALTROX_WALL_HANGING_SIGN)
+
+                .add(ModBlocks.PETRIFIED_VALTROX_WALL_HANGING_SIGN);
     }
 }
