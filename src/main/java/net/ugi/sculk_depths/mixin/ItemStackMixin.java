7eccf08e51c6a28004d427f1fc4c81a57cad34ad
gemsb <85642836+gemsb@users.noreply.github.com>
2024-01-05 17:46:35 +0100

    yey

--- src/main/java/net/ugi/sculk_depths/mixin/ItemStackMixin.java
+++ src/main/java/net/ugi/sculk_depths/mixin/ItemStackMixin.java
@@ -1,25 +1,27 @@
 package net.ugi.sculk_depths.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.mojang.util.UUIDTypeAdapter;
+import net.fabricmc.fabric.api.item.v1.FabricItemStack;
+import net.minecraft.command.argument.UuidArgumentType;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.Uuids;
+import org.apache.logging.log4j.core.config.plugins.convert.TypeConverters;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 import java.util.UUID;
 
 @Mixin(ItemStack.class)
-public class ItemStackMixin extends Item{
-    public ItemStackMixin(Settings settings) {
-        super(settings);
-    }
+public class ItemStackMixin{
 
     @ModifyExpressionValue(
             method = "getTooltip",
-            @At(value = "INVOKE", target = "Lnet/minecraft/entity/attribute/EntityAttributeModifier;getId()Ljava/util/UUID;")
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/attribute/EntityAttributeModifier;getId()Ljava/util/UUID;")
 
     )
-    private static UUID fixBug (UUID original){
+    private static UUID fixBug(UUID original){
         if (original.equals(Item.ATTACK_DAMAGE_MODIFIER_ID)) {
             return Item.ATTACK_DAMAGE_MODIFIER_ID;
         }
