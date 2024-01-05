6be0f27737305e12b265e963d7ddb70a40743075
warior456 <matteo.feyaerts@gmail.com>
2024-01-05 15:59:10 +0100

    add ost-1 (third song)

--- src/main/java/net/ugi/sculk_depths/sound/ModSounds.java
+++ src/main/java/net/ugi/sculk_depths/sound/ModSounds.java
@@ -11,11 +11,13 @@ public class ModSounds {
     public static final Identifier SCULK_DEPTHS_MAIN_SOUNDTRACK = new Identifier(SculkDepths.MOD_ID, "music.sculk_depths.main_soundtrack");
     public static final Identifier SCULK_DEPTHS_MAIN_THEME = new Identifier("sculk_depths:music.sculk_depths.main_theme");
     public static final Identifier SCULK_DEPTHS_ZINNIA = new Identifier("sculk_depths:music.sculk_depths.zinnia");
+    public static final Identifier SCULK_DEPTHS_OST_1 = new Identifier("sculk_depths:music.sculk_depths.ost-1");
     public static final Identifier AMBIENT_WIND_ADDITIONS = new Identifier("sculk_depths:ambient.wind.additions");
     public static final Identifier AMBIENT_CAVE_ADDITIONS = new Identifier("sculk_depths:ambient.cave.additions");
     public static SoundEvent SCULK_DEPTHS_MAIN_SOUNDTRACK_EVENT = SoundEvent.of(SCULK_DEPTHS_MAIN_SOUNDTRACK);
     public static SoundEvent SCULK_DEPTHS_MAIN_THEME_EVENT = SoundEvent.of(SCULK_DEPTHS_MAIN_THEME);
     public static SoundEvent SCULK_DEPTHS_ZINNIA_EVENT = SoundEvent.of(SCULK_DEPTHS_ZINNIA);
+    public static SoundEvent SCULK_DEPTHS_OST_1_EVENT = SoundEvent.of(SCULK_DEPTHS_OST_1);
     public static SoundEvent AMBIENT_WIND_ADDITIONS_EVENT = SoundEvent.of(AMBIENT_WIND_ADDITIONS);
     public static SoundEvent AMBIENT_CAVE_ADDITIONS_EVENT = SoundEvent.of(AMBIENT_CAVE_ADDITIONS);
 
@@ -23,6 +25,7 @@ public class ModSounds {
         Registry.register(Registries.SOUND_EVENT, ModSounds.SCULK_DEPTHS_MAIN_SOUNDTRACK, SCULK_DEPTHS_MAIN_SOUNDTRACK_EVENT);
         Registry.register(Registries.SOUND_EVENT, ModSounds.SCULK_DEPTHS_MAIN_THEME, SCULK_DEPTHS_MAIN_THEME_EVENT);
         Registry.register(Registries.SOUND_EVENT, ModSounds.SCULK_DEPTHS_ZINNIA, SCULK_DEPTHS_ZINNIA_EVENT);
+        Registry.register(Registries.SOUND_EVENT, ModSounds.SCULK_DEPTHS_OST_1, SCULK_DEPTHS_OST_1_EVENT);
         Registry.register(Registries.SOUND_EVENT, ModSounds.AMBIENT_WIND_ADDITIONS, AMBIENT_WIND_ADDITIONS_EVENT);
         Registry.register(Registries.SOUND_EVENT, ModSounds.AMBIENT_CAVE_ADDITIONS, AMBIENT_CAVE_ADDITIONS_EVENT);
     }
