3637cd7233f2848ef93bb25f5cde4ce118f1d45e
warior456 <matteo.feyaerts@gmail.com>
2024-01-07 00:12:19 +0100

    some raycast (not final)

--- src/main/java/net/ugi/sculk_depths/block/custom/LesterSpawningBlock.java
+++ src/main/java/net/ugi/sculk_depths/block/custom/LesterSpawningBlock.java
@@ -2,6 +2,7 @@ package net.ugi.sculk_depths.block.custom;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -18,8 +19,21 @@ public class LesterSpawningBlock extends Block {
     public void onBroken(WorldAccess world, BlockPos pos, BlockState state) {
         if(world.isClient())return;
         Entity entity = new LesterEntity(ModEntities.LESTER, (World) world);
-
-        entity.setPosition(pos.getX(),pos.getY(),pos.getZ());
+        System.out.println("yay");
+        int y = raycastup(pos, (World)world);
+        entity.setPosition(pos.getX(), pos.up(y).getY(), pos.getZ());
         world.spawnEntity(entity);
+    }
+
+    public int raycastup (BlockPos pos, World world){
+        Block block = world.getBlockState(pos.up()).getBlock();
+        int i= 0;
+        System.out.println(i);
+        while (block == Blocks.AIR && !(i > 20)){
+            System.out.println(i);
+                i++;
+                block = world.getBlockState(pos.up(i)).getBlock();
+        }
+        return i;
     }
 }
