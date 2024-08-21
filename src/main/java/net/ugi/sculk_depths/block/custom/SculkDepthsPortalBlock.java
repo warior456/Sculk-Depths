9d2d0b2a202e6cb0198afa492ec86a4c64a6371a
Matteo_fey <matteo.feyaerts@gmail.Com>
2024-08-21 13:13:37 +0200

    portal teleporting (no effects)

--- src/main/java/net/ugi/sculk_depths/block/custom/SculkDepthsPortalBlock.java
+++ src/main/java/net/ugi/sculk_depths/block/custom/SculkDepthsPortalBlock.java
@@ -1,15 +1,16 @@
 package net.ugi.sculk_depths.block.custom;
 
-import com.mojang.logging.LogUtils;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.block.*;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.fluid.Fluid;
 import net.minecraft.item.ItemStack;
-import net.minecraft.particle.BlockStateParticleEffect;
+import net.minecraft.particle.ParticleEffect;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.particle.SculkChargeParticleEffect;
+import net.minecraft.registry.RegistryKey;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
@@ -18,15 +19,17 @@ import net.minecraft.state.property.EnumProperty;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.*;
 import net.minecraft.world.dimension.NetherPortal;
-import net.ugi.sculk_depths.block.ModBlocks;
-import net.ugi.sculk_depths.portal.PortalUtils;
-import org.jetbrains.annotations.Nullable;
 
-public class SculkDepthsPortalBlock extends Block implements Portal {
+import net.ugi.sculk_depths.particle.ModParticleTypes;
+import net.ugi.sculk_depths.world.dimension.ModDimensions;
+
+
+public class SculkDepthsPortalBlock extends Block implements Portal{
     public static final EnumProperty<Direction.Axis> AXIS = Properties.HORIZONTAL_AXIS;
     protected static final VoxelShape X_SHAPE = Block.createCuboidShape(0.0D, 0.0D, 6.0D, 16.0D, 16.0D, 10.0D);
     protected static final VoxelShape Z_SHAPE = Block.createCuboidShape(6.0D, 0.0D, 0.0D, 10.0D, 16.0D, 16.0D);
@@ -63,45 +66,49 @@ public class SculkDepthsPortalBlock extends Block implements Portal {
         return ItemStack.EMPTY;
     }
 
-    @Override
     @Environment(EnvType.CLIENT)
+    @Override
     public void randomDisplayTick(BlockState state, World world, BlockPos pos, Random random) {
-        /*if (random.nextInt(100) == 0) {
-            world.playSound((double) pos.getX() + 0.5D, (double) pos.getY() + 0.5D, (double) pos.getZ() + 0.5D, SoundEvents.BLOCK_PORTAL_AMBIENT, SoundCategory.BLOCKS, 0.5F, random.nextFloat() * 0.4F + 0.8F, false);
+        if (random.nextInt(100) == 0) {
+            world.playSound(
+                    (double)pos.getX() + 0.5,
+                    (double)pos.getY() + 0.5,
+                    (double)pos.getZ() + 0.5,
+                    SoundEvents.BLOCK_PORTAL_AMBIENT,
+                    SoundCategory.BLOCKS,
+                    0.5F,
+                    random.nextFloat() * 0.4F + 0.8F,
+                    false
+            );
         }
 
-        for (int i = 0; i < 4; ++i) {
-            double d = (double) pos.getX() + random.nextDouble();
-            double e = (double) pos.getY() + random.nextDouble();
-            double f = (double) pos.getZ() + random.nextDouble();
-            double g = ((double) random.nextFloat() - 0.5) * 0.5;
-            double h = ((double) random.nextFloat() - 0.5) * 0.5;
-            double j = ((double) random.nextFloat() - 0.5) * 0.5;
+        for (int i = 0; i < 4; i++) {
+            double d = (double)pos.getX() + random.nextDouble();
+            double e = (double)pos.getY() + random.nextDouble();
+            double f = (double)pos.getZ() + random.nextDouble();
+            double g = ((double)random.nextFloat() - 0.5) * 0.5;
+            double h = ((double)random.nextFloat() - 0.5) * 0.5;
+            double j = ((double)random.nextFloat() - 0.5) * 0.5;
             int k = random.nextInt(2) * 2 - 1;
-
-            if (state.get(AXIS) == Direction.Axis.Y) {
-                h = random.nextFloat() * 2.0f * (float) k;
+            if (!world.getBlockState(pos.west()).isOf(this) && !world.getBlockState(pos.east()).isOf(this)) {
+                d = (double)pos.getX() + 0.5 + 0.25 * (double)k;
+                g = (double)(random.nextFloat() * 2.0F * (float)k);
             } else {
-                if (world.getBlockState(pos.west()).isOf(this) || world.getBlockState(pos.east()).isOf(this)) {
-                    f = (double) pos.getZ() + 0.5 + 0.25 * (double) k;
-                    j = random.nextFloat() * 2.0f * (float) k;
-                } else {
-                    d = (double) pos.getX() + 0.5 + 0.25 * (double) k;
-                    g = random.nextFloat() * 2.0f * (float) k;
-                }
+                f = (double)pos.getZ() + 0.5 + 0.25 * (double)k;
+                j = (double)(random.nextFloat() * 2.0F * (float)k);
             }
-            world.addParticle(new BlockStateParticleEffect(CustomPortalsModClient.CUSTOMPORTALPARTICLE, getPortalBase(world, pos).getDefaultState()), d, e, f, g, h, j);
-        }*/
+
+            world.addParticle((ParticleEffect) ModParticleTypes.PENEBRIUM_SPORES, d, e, f, g, h, j);
+        }
     }
 
     @Override
     public void onEntityCollision(BlockState state, World world, BlockPos pos, Entity entity) {
-        PortalUtils.attemptTeleport(world, entity, world.getBlockState(pos));
-/*        if (entity.canUsePortals(false)) {
+        if(world.isClient())return;
+        if(entity.canUsePortals(true)){
             entity.tryUsePortal(this, pos);
-        }*/
+        }
     }
-
 
 
     @Override
@@ -113,12 +120,28 @@ public class SculkDepthsPortalBlock extends Block implements Portal {
         }
     }
 
+
     @Override
-    @Nullable
-    public  TeleportTarget createTeleportTarget(ServerWorld world, Entity entity, BlockPos pos) {
-        /*return CustomTeleporter.createTeleportTarget(world, entity, getPortalBase(world, pos), pos);*/
-        return null;
+    public TeleportTarget createTeleportTarget(ServerWorld world, Entity entity, BlockPos blockPos) {
+        RegistryKey<World> registryKey = world.getRegistryKey() == ModDimensions.SCULK_DEPTHS_LEVEL_KEY ? World.OVERWORLD : ModDimensions.SCULK_DEPTHS_LEVEL_KEY;
+        ServerWorld serverWorld = world.getServer().getWorld(registryKey);
+        if (serverWorld == null) {
+            return null;
+        } else {
+            Vec3d vec3d = Vec3d.of(blockPos);
+            vec3d = vec3d.add(0.5f, 0, 0.5f);
+
+            return new TeleportTarget(
+                    serverWorld,
+                    vec3d,
+                    entity.getVelocity(),
+                    entity.getYaw(),
+                    entity.getPitch(),
+                    TeleportTarget.SEND_TRAVEL_THROUGH_PORTAL_PACKET.then(TeleportTarget.ADD_PORTAL_CHUNK_TICKET)
+            );
+        }
     }
+
 
     @Override
     public Effect getPortalEffect() {
--- src/main/java/net/ugi/sculk_depths/particle/ModParticleTypes.java
+++ src/main/java/net/ugi/sculk_depths/particle/ModParticleTypes.java
@@ -2,22 +2,19 @@ package net.ugi.sculk_depths.particle;
 
 
 import net.fabricmc.fabric.api.particle.v1.FabricParticleTypes;
-import net.minecraft.particle.BlockStateParticleEffect;
-import net.minecraft.particle.ParticleEffect;
 import net.minecraft.particle.ParticleType;
 import net.minecraft.particle.SimpleParticleType;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-import net.minecraft.util.Identifier;
 import net.ugi.sculk_depths.SculkDepths;
 
 public class ModParticleTypes {
 
-    public static final ParticleType PENEBRIUM_SPORES = FabricParticleTypes.simple();
+    public static final ParticleType<SimpleParticleType> PENEBRIUM_SPORES = FabricParticleTypes.simple();
 
-    public static final ParticleType AURIC_SPORES = FabricParticleTypes.simple();
-    public static final ParticleType SURFACE_WIND = FabricParticleTypes.simple();
-    public static final ParticleType CAVE_FALLING_PARTICLE = FabricParticleTypes.simple();
+    public static final ParticleType<SimpleParticleType> AURIC_SPORES = FabricParticleTypes.simple();
+    public static final ParticleType<SimpleParticleType> SURFACE_WIND = FabricParticleTypes.simple();
+    public static final ParticleType<SimpleParticleType> CAVE_FALLING_PARTICLE = FabricParticleTypes.simple();
 
     public static void registerModParticles() {
         Registry.register(Registries.PARTICLE_TYPE, SculkDepths.identifier( "penebrium_spores"), PENEBRIUM_SPORES);
