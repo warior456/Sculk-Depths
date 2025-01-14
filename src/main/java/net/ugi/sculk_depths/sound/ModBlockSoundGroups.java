701b8d8b1f4274a1ba4afcb63a2bb2948afbb519
Matteo_fey <matteo.feyaerts@gmail.Com>
2025-01-14 19:12:15 +0100

    add amalgamite custom break sound (with pitch variation)

---
+++ src/main/java/net/ugi/sculk_depths/sound/ModBlockSoundGroups.java
@@ -0,0 +1,10 @@
+package net.ugi.sculk_depths.sound;
+
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.sound.SoundEvents;
+
+import static net.ugi.sculk_depths.sound.ModSounds.*;
+
+public class ModBlockSoundGroups {
+    public static BlockSoundGroup AMALGAMITE = new BlockSoundGroup(1,1,BLOCK_AMALGAMITE_BREAK_EVENT, SoundEvents.BLOCK_DEEPSLATE_STEP, SoundEvents.BLOCK_DEEPSLATE_PLACE, SoundEvents.BLOCK_DEEPSLATE_HIT, SoundEvents.BLOCK_DEEPSLATE_FALL);
+}
