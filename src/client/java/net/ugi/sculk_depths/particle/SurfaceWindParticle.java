b5c52b797d9b9c2bf11d5fa8f3472a3c7be433c9
Matteo_fey <matteo.feyaerts@gmail.Com>
2025-01-22 01:26:18 +0100

    wind basics

--- src/client/java/net/ugi/sculk_depths/particle/SurfaceWindParticle.java
+++ src/client/java/net/ugi/sculk_depths/particle/SurfaceWindParticle.java
@@ -10,6 +10,7 @@ import net.minecraft.client.world.ClientWorld;
 import net.minecraft.particle.SimpleParticleType;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.Heightmap;
+import net.ugi.sculk_depths.sound.ConditionalSoundPlayerClient;
 import org.jetbrains.annotations.Nullable;
 
 public class SurfaceWindParticle extends AscendingParticle {
@@ -31,14 +32,14 @@ public class SurfaceWindParticle extends AscendingParticle {
 
         @Nullable
         @Override
-        public Particle createParticle(SimpleParticleType parameters, ClientWorld clientWorld, double x, double y, double z, double g, double h, double i) {
+        public Particle createParticle(SimpleParticleType parameters, ClientWorld clientWorld, double x, double y, double z, double vel_x, double h, double vel_z) {
             BlockPos pos = new BlockPos((int) x, (int) y, (int) z);
             // Check if the top Y position is greater than y, if so return null to spawn no particle
             if (clientWorld.getTopPosition(Heightmap.Type.MOTION_BLOCKING, pos).getY() > y) {
                 return null;
             }
 
-            SurfaceWindParticle particle = new SurfaceWindParticle(clientWorld, x, y, z, 0.5f, 0.05f, 0.5f, 0.4f, -0.05f, 0.4f, 1.0f, this.spriteProvider, 0.9f, 20, 0.002f, true);
+            SurfaceWindParticle particle = new SurfaceWindParticle(clientWorld, x, y, z, 0.5f, 0.05f, 0.5f, ConditionalSoundPlayerClient.getWindX(), -0.05f, ConditionalSoundPlayerClient.getWindZ(), 1.0f, this.spriteProvider, 0.9f, 20, 0.002f, true);
             return particle;
         }
 
