b9543c7a513e26e7c4458a78ab59755ba433197c
Proctored Games <proctoredGames@hotmail.com>
2025-01-25 01:09:44 -0800

    dried valtrox now both burns faster and catches on fire faster

--- src/main/java/net/ugi/sculk_depths/util/ModFlammableBlocks.java
+++ src/main/java/net/ugi/sculk_depths/util/ModFlammableBlocks.java
@@ -6,34 +6,34 @@ import net.ugi.sculk_depths.block.ModBlocks;
 
 public class ModFlammableBlocks {
     public static void registerFlammables() {
-        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.VALTROX_LOG, 5, 20);
+        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.VALTROX_LOG, 10, 20);
         FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.STRIPPED_VALTROX_LOG, 5, 20);
-        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.VALTROX_WOOD, 5, 20);
-        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.VALTROX_PLANKS, 5, 20);
-        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.VALTROX_STAIRS, 5, 20);
-        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.VALTROX_SLAB, 5, 20);
-        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.VALTROX_FENCE, 5, 20);
-        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.VALTROX_FENCE_GATE, 5, 20);
-        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.VALTROX_DOOR, 5, 20);
-        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.VALTROX_TRAPDOOR, 5, 20);
-        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.VALTROX_PRESSURE_PLATE, 5, 20);
-        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.VALTROX_SIGN, 5, 20);
-        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.VALTROX_HANGING_SIGN, 5, 20);
-        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.VALTROX_WALL_HANGING_SIGN, 5, 20);
+        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.VALTROX_WOOD, 10, 20);
+        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.VALTROX_PLANKS, 10, 20);
+        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.VALTROX_STAIRS, 10, 20);
+        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.VALTROX_SLAB, 10, 20);
+        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.VALTROX_FENCE, 10, 20);
+        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.VALTROX_FENCE_GATE, 10, 20);
+        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.VALTROX_DOOR, 10, 20);
+        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.VALTROX_TRAPDOOR, 10, 20);
+        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.VALTROX_PRESSURE_PLATE, 10, 20);
+        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.VALTROX_SIGN, 10, 20);
+        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.VALTROX_HANGING_SIGN, 10, 20);
+        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.VALTROX_WALL_HANGING_SIGN, 10, 20);
 
-        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.DRIED_VALTROX_LOG, 3, 20);
-        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.STRIPPED_DRIED_VALTROX_LOG, 3, 20);
-        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.DRIED_VALTROX_WOOD, 3, 20);
-        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.DRIED_VALTROX_PLANKS, 3, 20);
-        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.DRIED_VALTROX_STAIRS, 3, 20);
-        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.DRIED_VALTROX_SLAB, 3, 20);
-        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.DRIED_VALTROX_FENCE, 3, 20);
-        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.DRIED_VALTROX_FENCE_GATE, 3, 20);
-        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.DRIED_VALTROX_DOOR, 3, 20);
-        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.DRIED_VALTROX_TRAPDOOR, 3, 20);
-        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.DRIED_VALTROX_PRESSURE_PLATE, 3, 20);
-        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.DRIED_VALTROX_SIGN, 3, 20);
-        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.DRIED_VALTROX_HANGING_SIGN, 3, 20);
-        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.DRIED_VALTROX_WALL_HANGING_SIGN, 3, 20);
+        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.DRIED_VALTROX_LOG, 5, 40);
+        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.STRIPPED_DRIED_VALTROX_LOG, 5, 40);
+        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.DRIED_VALTROX_WOOD, 5, 40);
+        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.DRIED_VALTROX_PLANKS, 5, 40);
+        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.DRIED_VALTROX_STAIRS, 5, 40);
+        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.DRIED_VALTROX_SLAB, 5, 40);
+        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.DRIED_VALTROX_FENCE, 5, 40);
+        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.DRIED_VALTROX_FENCE_GATE, 5, 40);
+        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.DRIED_VALTROX_DOOR, 5, 40);
+        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.DRIED_VALTROX_TRAPDOOR, 5, 40);
+        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.DRIED_VALTROX_PRESSURE_PLATE, 5, 40);
+        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.DRIED_VALTROX_SIGN, 5, 40);
+        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.DRIED_VALTROX_HANGING_SIGN, 5, 40);
+        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.DRIED_VALTROX_WALL_HANGING_SIGN, 5, 40);
     }
 }
