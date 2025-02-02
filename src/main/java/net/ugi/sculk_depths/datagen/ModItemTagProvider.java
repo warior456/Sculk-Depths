3ac4ab2a0f8b59ebb09ed10ba44987698e52ea70
Matteo_fey <matteo.feyaerts@gmail.Com>
2025-02-02 19:26:55 +0100

    valtrox itemTags

--- src/main/java/net/ugi/sculk_depths/datagen/ModItemTagProvider.java
+++ src/main/java/net/ugi/sculk_depths/datagen/ModItemTagProvider.java
@@ -2,6 +2,7 @@ package net.ugi.sculk_depths.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.minecraft.item.Item;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.registry.tag.ItemTags;
 import net.ugi.sculk_depths.block.ModBlocks;
@@ -80,5 +81,176 @@ public class ModItemTagProvider extends FabricTagProvider.ItemTagProvider {
 
         getOrCreateTagBuilder(ItemTags.FOOT_ARMOR_ENCHANTABLE)
                 .add(ModItems.QUAZARITH_BOOTS);
+
+        // ------------------- saplings -------------------
+        getOrCreateTagBuilder(ItemTags.SAPLINGS)
+                .add(ModBlocks.VALTROX_SAPLING.asItem());
+
+        // ------------------- logs (any) -------------------
+        getOrCreateTagBuilder(ItemTags.LOGS)
+                .add(ModBlocks.VALTROX_LOG.asItem())
+                .add(ModBlocks.VALTROX_WOOD.asItem())
+                .add(ModBlocks.STRIPPED_VALTROX_LOG.asItem())
+                .add(ModBlocks.STRIPPED_VALTROX_WOOD.asItem())
+
+                .add(ModBlocks.COATED_VALTROX_LOG.asItem())
+                .add(ModBlocks.COATED_VALTROX_WOOD.asItem())
+                .add(ModBlocks.COATED_STRIPPED_VALTROX_LOG.asItem())
+                .add(ModBlocks.COATED_STRIPPED_VALTROX_WOOD.asItem())
+
+                .add(ModBlocks.DRIED_VALTROX_LOG.asItem())
+                .add(ModBlocks.DRIED_VALTROX_WOOD.asItem())
+                .add(ModBlocks.STRIPPED_DRIED_VALTROX_LOG.asItem())
+                .add(ModBlocks.STRIPPED_DRIED_VALTROX_WOOD.asItem())
+
+                .add(ModBlocks.PETRIFIED_VALTROX_LOG.asItem())
+                .add(ModBlocks.PETRIFIED_VALTROX_WOOD.asItem())
+                .add(ModBlocks.STRIPPED_PETRIFIED_VALTROX_LOG.asItem())
+                .add(ModBlocks.STRIPPED_PETRIFIED_VALTROX_WOOD.asItem());
+
+        // ------------------- logs (burnable) -------------------
+        getOrCreateTagBuilder(ItemTags.LOGS_THAT_BURN)
+                .add(ModBlocks.VALTROX_LOG.asItem())
+                .add(ModBlocks.VALTROX_WOOD.asItem())
+                .add(ModBlocks.STRIPPED_VALTROX_LOG.asItem())
+                .add(ModBlocks.STRIPPED_VALTROX_WOOD.asItem())
+
+                .add(ModBlocks.COATED_VALTROX_LOG.asItem())
+                .add(ModBlocks.COATED_VALTROX_WOOD.asItem())
+                .add(ModBlocks.COATED_STRIPPED_VALTROX_LOG.asItem())
+                .add(ModBlocks.COATED_STRIPPED_VALTROX_WOOD.asItem())
+
+                .add(ModBlocks.DRIED_VALTROX_LOG.asItem())
+                .add(ModBlocks.DRIED_VALTROX_WOOD.asItem())
+                .add(ModBlocks.STRIPPED_DRIED_VALTROX_LOG.asItem())
+                .add(ModBlocks.STRIPPED_DRIED_VALTROX_WOOD.asItem());
+
+        // ------------------- planks -------------------
+        getOrCreateTagBuilder(ItemTags.PLANKS)
+                .add(ModBlocks.VALTROX_PLANKS.asItem())
+                .add(ModBlocks.COATED_VALTROX_PLANKS.asItem())
+                .add(ModBlocks.DRIED_VALTROX_PLANKS.asItem());
+
+        // ------------------- stairs (any) ------------------- //todo the rest
+        getOrCreateTagBuilder(ItemTags.STAIRS)
+                .add(ModBlocks.VALTROX_STAIRS.asItem())
+                .add(ModBlocks.COATED_VALTROX_STAIRS.asItem())
+                .add(ModBlocks.DRIED_VALTROX_STAIRS.asItem())
+                .add(ModBlocks.PETRIFIED_VALTROX_STAIRS.asItem());
+
+        // ------------------- stairs (burnable) -------------------
+        getOrCreateTagBuilder(ItemTags.WOODEN_STAIRS)
+                .add(ModBlocks.VALTROX_STAIRS.asItem())
+                .add(ModBlocks.COATED_VALTROX_STAIRS.asItem())
+                .add(ModBlocks.DRIED_VALTROX_STAIRS.asItem());
+
+        // ------------------- slabs (any) ------------------- //todo the rest
+        getOrCreateTagBuilder(ItemTags.SLABS)
+                .add(ModBlocks.VALTROX_SLAB.asItem())
+                .add(ModBlocks.COATED_VALTROX_SLAB.asItem())
+                .add(ModBlocks.DRIED_VALTROX_SLAB.asItem())
+                .add(ModBlocks.PETRIFIED_VALTROX_SLAB.asItem());
+
+        // ------------------- slabs (burnable) -------------------
+        getOrCreateTagBuilder(ItemTags.WOODEN_SLABS)
+                .add(ModBlocks.VALTROX_SLAB.asItem())
+                .add(ModBlocks.COATED_VALTROX_SLAB.asItem())
+                .add(ModBlocks.DRIED_VALTROX_SLAB.asItem());
+
+        // ------------------- fences (any) -------------------
+        getOrCreateTagBuilder(ItemTags.FENCES)
+                .add(ModBlocks.VALTROX_FENCE.asItem())
+                .add(ModBlocks.COATED_VALTROX_FENCE.asItem())
+                .add(ModBlocks.DRIED_VALTROX_FENCE.asItem());
+
+        // ------------------- fences (burnable) -------------------
+        getOrCreateTagBuilder(ItemTags.WOODEN_FENCES)
+                .add(ModBlocks.VALTROX_FENCE.asItem())
+                .add(ModBlocks.COATED_VALTROX_FENCE.asItem())
+                .add(ModBlocks.DRIED_VALTROX_FENCE.asItem());
+
+        // ------------------- fence_gates (burnable?) -------------------
+        getOrCreateTagBuilder(ItemTags.FENCE_GATES)
+                .add(ModBlocks.VALTROX_FENCE_GATE.asItem())
+                .add(ModBlocks.COATED_VALTROX_FENCE_GATE.asItem())
+                .add(ModBlocks.DRIED_VALTROX_FENCE_GATE.asItem())
+                .add(ModBlocks.PETRIFIED_VALTROX_WALL_GATE.asItem());
+
+        // ------------------- doors (any) -------------------
+        getOrCreateTagBuilder(ItemTags.DOORS)
+                .add(ModBlocks.VALTROX_DOOR.asItem())
+                .add(ModBlocks.COATED_VALTROX_DOOR.asItem())
+                .add(ModBlocks.DRIED_VALTROX_DOOR.asItem())
+                .add(ModBlocks.PETRIFIED_VALTROX_DOOR.asItem());
+
+        // ------------------- doors (burnable) -------------------
+        getOrCreateTagBuilder(ItemTags.WOODEN_DOORS)
+                .add(ModBlocks.VALTROX_DOOR.asItem())
+                .add(ModBlocks.COATED_VALTROX_DOOR.asItem())
+                .add(ModBlocks.DRIED_VALTROX_DOOR.asItem());
+
+        // ------------------- trapdoors (any) -------------------
+        getOrCreateTagBuilder(ItemTags.TRAPDOORS)
+                .add(ModBlocks.VALTROX_TRAPDOOR.asItem())
+                .add(ModBlocks.COATED_VALTROX_TRAPDOOR.asItem())
+                .add(ModBlocks.DRIED_VALTROX_TRAPDOOR.asItem())
+                .add(ModBlocks.PETRIFIED_VALTROX_TRAPDOOR.asItem());
+
+        // ------------------- trapdoors (any) -------------------
+        getOrCreateTagBuilder(ItemTags.WOODEN_TRAPDOORS)
+                .add(ModBlocks.VALTROX_TRAPDOOR.asItem())
+                .add(ModBlocks.COATED_VALTROX_TRAPDOOR.asItem())
+                .add(ModBlocks.DRIED_VALTROX_TRAPDOOR.asItem());
+
+        // ------------------- pressure_plates (wooden) -------------------
+        getOrCreateTagBuilder(ItemTags.WOODEN_PRESSURE_PLATES)
+                .add(ModBlocks.VALTROX_PRESSURE_PLATE.asItem())
+                .add(ModBlocks.COATED_VALTROX_PRESSURE_PLATE.asItem())
+                .add(ModBlocks.DRIED_VALTROX_PRESSURE_PLATE.asItem());
+
+        // ------------------- buttons (any) -------------------
+        getOrCreateTagBuilder(ItemTags.BUTTONS)
+                .add(ModBlocks.VALTROX_BUTTON.asItem())
+                .add(ModBlocks.COATED_VALTROX_BUTTON.asItem())
+                .add(ModBlocks.DRIED_VALTROX_BUTTON.asItem())
+                .add(ModBlocks.PETRIFIED_VALTROX_BUTTON.asItem());
+
+        // ------------------- buttons (wooden) -------------------
+        getOrCreateTagBuilder(ItemTags.WOODEN_BUTTONS)
+                .add(ModBlocks.VALTROX_BUTTON.asItem())
+                .add(ModBlocks.COATED_VALTROX_BUTTON.asItem())
+                .add(ModBlocks.DRIED_VALTROX_BUTTON.asItem());
+
+        // ------------------- signs (normal) -------------------
+        getOrCreateTagBuilder(ItemTags.SIGNS)
+                .add(ModBlocks.VALTROX_SIGN.asItem())
+                .add(ModBlocks.COATED_VALTROX_SIGN.asItem())
+                .add(ModBlocks.DRIED_VALTROX_SIGN.asItem())
+                .add(ModBlocks.PETRIFIED_VALTROX_SIGN.asItem());
+
+        // ------------------- hanging signs -------------------
+        getOrCreateTagBuilder(ItemTags.HANGING_SIGNS)
+                .add(ModBlocks.VALTROX_HANGING_SIGN.asItem())
+                .add(ModBlocks.COATED_VALTROX_HANGING_SIGN.asItem())
+                .add(ModBlocks.DRIED_VALTROX_HANGING_SIGN.asItem())
+                .add(ModBlocks.PETRIFIED_VALTROX_HANGING_SIGN.asItem());
+
+        // ------------------- non flammable wood -------------------
+        getOrCreateTagBuilder(ItemTags.NON_FLAMMABLE_WOOD)
+                .add(ModBlocks.PETRIFIED_VALTROX_LOG.asItem())
+                .add(ModBlocks.PETRIFIED_VALTROX_WOOD.asItem())
+                .add(ModBlocks.STRIPPED_PETRIFIED_VALTROX_LOG.asItem())
+                .add(ModBlocks.STRIPPED_PETRIFIED_VALTROX_WOOD.asItem())
+                .add(ModBlocks.PETRIFIED_VALTROX_SLATES.asItem())
+                .add(ModBlocks.PETRIFIED_VALTROX_STAIRS.asItem())
+                .add(ModBlocks.PETRIFIED_VALTROX_SLAB.asItem())
+                .add(ModBlocks.PETRIFIED_VALTROX_WALL.asItem())
+                .add(ModBlocks.PETRIFIED_VALTROX_WALL_GATE.asItem())
+                .add(ModBlocks.PETRIFIED_VALTROX_DOOR.asItem())
+                .add(ModBlocks.PETRIFIED_VALTROX_TRAPDOOR.asItem())
+                .add(ModBlocks.PETRIFIED_VALTROX_PRESSURE_PLATE.asItem())
+                .add(ModBlocks.PETRIFIED_VALTROX_BUTTON.asItem())
+                .add(ModBlocks.PETRIFIED_VALTROX_SIGN.asItem())
+                .add(ModBlocks.PETRIFIED_VALTROX_HANGING_SIGN.asItem());
     }
 }
