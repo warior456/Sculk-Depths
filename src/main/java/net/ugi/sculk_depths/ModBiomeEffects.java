db430435197e9dab8057f5470107eb3a6b569a4f
Matteo_fey <matteo.feyaerts@gmail.Com>
2025-04-14 21:14:29 +0200

    import cleanup

--- src/main/java/net/ugi/sculk_depths/ModBiomeEffects.java
+++ src/main/java/net/ugi/sculk_depths/ModBiomeEffects.java
@@ -1,31 +1,16 @@
 package net.ugi.sculk_depths;
 
 
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.damage.DamageSources;
-import net.minecraft.entity.damage.DamageType;
-import net.minecraft.entity.damage.DamageTypes;
-import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.entity.effect.StatusEffects;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.registry.RegistryKey;
-import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.stat.*;
+import net.minecraft.util.Identifier;
 import net.ugi.sculk_depths.entity.effect.ModStatusEffects;
-import net.ugi.sculk_depths.item.ModItems;
 import net.ugi.sculk_depths.world.biome.ModBiomes;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class ModBiomeEffects implements ServerTickEvents.StartWorldTick{
